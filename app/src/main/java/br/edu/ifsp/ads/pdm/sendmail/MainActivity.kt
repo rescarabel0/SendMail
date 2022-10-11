d222090bf07796ec36f916348b74f53906cf864e
rescarabelo <rafael@scenario.ind.br>
2022-10-11 09:22:54 -0300

    intents

--- app/src/main/java/br/edu/ifsp/ads/pdm/sendmail/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/ads/pdm/sendmail/MainActivity.kt
@@ -1,5 +1,8 @@
 package br.edu.ifsp.ads.pdm.sendmail
 
+import android.content.Intent
+import android.content.Intent.*
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.ads.pdm.sendmail.databinding.ActivityMainBinding
@@ -12,5 +15,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        amb.clearBtn.setOnClickListener {
+            with(amb) {
+                toEt.setText("")
+                ccEt.setText("")
+                bccEt.setText("")
+                subjectEt.setText("")
+                messageEt.setText("")
+            }
+        }
+
+        amb.sendBtn.setOnClickListener {
+            val sendMailIntent = Intent(ACTION_SENDTO)
+
+            with(sendMailIntent) {
+                with(amb) {
+                    putExtra(EXTRA_EMAIL, arrayOf(toEt.text.toString()))
+                    putExtra(EXTRA_CC, arrayOf(ccEt.text.toString()))
+                    putExtra(EXTRA_BCC, arrayOf(bccEt.text.toString()))
+                    putExtra(EXTRA_SUBJECT, arrayOf(subjectEt.text.toString()))
+                    putExtra(EXTRA_TEXT, arrayOf(messageEt.text.toString()))
+                }
+                type = "message/rfc822"
+                data = Uri.parse("mailto:")
+            }
+
+            val chooserIntent = Intent(ACTION_CHOOSER)
+            chooserIntent.putExtra(EXTRA_INTENT, sendMailIntent)
+
+            startActivity(chooserIntent)
+        }
     }
 }
