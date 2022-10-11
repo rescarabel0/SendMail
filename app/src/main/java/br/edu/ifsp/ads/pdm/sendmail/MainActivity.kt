3ab3f97910ed7574869f7e43a5f7f7487849c91d
rescarabelo <rafael@scenario.ind.br>
2022-10-11 09:07:42 -0300

    view

--- app/src/main/java/br/edu/ifsp/ads/pdm/sendmail/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/ads/pdm/sendmail/MainActivity.kt
@@ -1,11 +1,16 @@
 package br.edu.ifsp.ads.pdm.sendmail
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.ads.pdm.sendmail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
     }
 }
